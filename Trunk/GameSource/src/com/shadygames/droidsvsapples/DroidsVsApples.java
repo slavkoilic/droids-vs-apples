6f7f0ce40b81f24cce57f5d97d40c8194a57bb13
tboling1 <tboling1@5feed978-1b4f-e323-4274-6f54b08fff53>
2011-05-02 02:54:56 +0000

    Display the loading screen now.  This is just a place holder for now, but it will be updated later.

--- Trunk/GameSource/src/com/shadygames/droidsvsapples/DroidsVsApples.java
+++ Trunk/GameSource/src/com/shadygames/droidsvsapples/DroidsVsApples.java
@@ -4,23 +4,19 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DroidsVsApples implements ApplicationListener {
-        private Mesh mesh;
-
+        
+        private Texture loadingScreen;
+        private SpriteBatch spriteBatch;
         @Override
         public void create() {
-                if (mesh == null) {
-                        mesh = new Mesh(true, 3, 3, 
-                                new VertexAttribute(Usage.Position, 3, "a_position"));          
-
-                        mesh.setVertices(new float[] { -0.5f, -0.5f, 0,
-                                                       0.5f, -0.5f, 0,
-                                                       0, 0.5f, 0 });   
-                        mesh.setIndices(new short[] { 0, 1, 2 });                       
-                }
+                loadingScreen = new Texture(Gdx.files.internal("Images/LoadingScreen512x512.png"));
+                spriteBatch = new SpriteBatch();
         }
 
         @Override
@@ -32,11 +28,15 @@ public class DroidsVsApples implements ApplicationListener {
         @Override
         public void render() {
                 Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-                mesh.render(GL10.GL_TRIANGLES, 0, 3);
+                spriteBatch.begin();
+                spriteBatch.draw(loadingScreen, 0,0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),0,0,480,320,false,false);
+                spriteBatch.end();
         }
 
         @Override
-        public void resize(int width, int height) { }
+        public void resize(int width, int height) {
+        	spriteBatch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+        }
 
         @Override
         public void resume() { }
