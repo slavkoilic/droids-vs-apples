57cbbe903b51f54367b286aae4bacc6147f698ef
tboling1 <tboling1@gmail.com>
2011-10-01 05:07:07 +0000

    Added loading screen and gson json parser.

--- Trunk/DesktopProject/src/com/shadygames/droidsvsapples/DroidsVsApplesDesktop.java
+++ Trunk/DesktopProject/src/com/shadygames/droidsvsapples/DroidsVsApplesDesktop.java
@@ -1,7 +1,13 @@
 package com.shadygames.droidsvsapples;
 
 import com.badlogic.gdx.backends.jogl.JoglApplication;
+import com.shadygames.droidsvsapples.EntryPoint.DroidsVsApples;
 
+/**
+ * Desktop version of the game.
+ * @author Travis
+ *
+ */
 public class DroidsVsApplesDesktop {
         public static void main (String[] argv) {
                 new JoglApplication(new DroidsVsApples(), "Droids Vs Apples", 480, 320, false);               
