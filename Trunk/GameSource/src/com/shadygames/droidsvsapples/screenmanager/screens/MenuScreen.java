96ca4f0337c80d575df91ce870ba7a84497d1e2f
tboling1 <tboling1@gmail.com>
2011-10-01 05:05:55 +0000

    Added webservice functionality.

--- Trunk/GameSource/src/com/shadygames/droidsvsapples/screenmanager/screens/MenuScreen.java
+++ Trunk/GameSource/src/com/shadygames/droidsvsapples/screenmanager/screens/MenuScreen.java
@@ -3,6 +3,9 @@ package com.shadygames.droidsvsapples.screenmanager.screens;
 import com.badlogic.gdx.math.Rectangle;
 import com.shadygames.droidsvsapples.screenmanager.ScreenManager;
 import com.shadygames.droidsvsapples.screenmanager.screens.components.MenuButton;
+import com.shadygames.droidsvsapples.webservices.backend.WebService.CallbackFunction;
+import com.shadygames.droidsvsapples.webservices.calls.login.ClientLoginManager;
+import com.shadygames.droidsvsapples.webservices.calls.login.LoginResponse;
 
 public class MenuScreen extends Screen implements MenuInterface {
 
@@ -14,10 +17,30 @@ public class MenuScreen extends Screen implements MenuInterface {
 		
 		int desiredWidth = 350;
 		
+		ClientLoginManager.AttemptLogin("test","pass", new LoginCallback());
+		
 		sprites.add(new MenuButton(new Rectangle(0,0,291,62),new Rectangle(ScreenManager.deviceWidth/2-desiredWidth/2,180,desiredWidth,62),"Images/ButtonUp.png","Images/ButtonDown.png","Play Game",gameTime,0,this));
 		sprites.add(new MenuButton(new Rectangle(0,0,291,62),new Rectangle(ScreenManager.deviceWidth/2-desiredWidth/2,120,desiredWidth,62),"Images/ButtonUp.png","Images/ButtonDown.png","Achievements",gameTime,1,this));
 		sprites.add(new MenuButton(new Rectangle(0,0,291,62),new Rectangle(ScreenManager.deviceWidth/2-desiredWidth/2,60,desiredWidth,62),"Images/ButtonUp.png","Images/ButtonDown.png","Options",gameTime,2,this));
 		sprites.add(new MenuButton(new Rectangle(0,0,291,62),new Rectangle(ScreenManager.deviceWidth/2-desiredWidth/2,0,desiredWidth,62),"Images/ButtonUp.png","Images/ButtonDown.png","Exit Game",gameTime,3,this));
+	}
+	
+	private class LoginCallback implements CallbackFunction<LoginResponse>
+	{
+		public LoginCallback()
+		{
+		}
+		
+		@Override
+		public void function(Exception ex, LoginResponse result) {
+			if ( ex != null )
+			{
+				System.out.println("Error!" + ex.toString());
+			} else {
+				System.out.println("in callback: " + result.message);
+			}
+		}
+		
 	}
 	
 	@Override
