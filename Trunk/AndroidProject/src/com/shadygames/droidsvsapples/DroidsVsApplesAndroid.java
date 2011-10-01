1cd70e120334ff9b8835bba3bfe7147892a3b765
tboling1 <tboling1@gmail.com>
2011-10-01 05:07:32 +0000

    Added icons and gson json parser.

--- Trunk/AndroidProject/src/com/shadygames/droidsvsapples/DroidsVsApplesAndroid.java
+++ Trunk/AndroidProject/src/com/shadygames/droidsvsapples/DroidsVsApplesAndroid.java
@@ -1,13 +1,26 @@
 package com.shadygames.droidsvsapples;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.shadygames.droidsvsapples.EntryPoint.DroidsVsApples;
 
+/**
+ * Android version of the game.
+ * @author Travis
+ *
+ */
 public class DroidsVsApplesAndroid extends AndroidApplication {
     @Override
         public void onCreate (Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 initialize(new DroidsVsApples(), false);               
         }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+      // ignore orientation/keyboard change
+      //super.onConfigurationChanged(newConfig);
+    }
 }
